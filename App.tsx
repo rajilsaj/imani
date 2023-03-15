0cbe68f255355ecc6c431cbeaadab6a42983876c
RajilSaj <svembe@gmail.com>
2023-03-15 01:48:59 -0400

    WIP

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,20 +6,28 @@
  * @format
  */
 
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Text, TextInput, View} from 'react-native';
 
 const HelloWorldApp = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
+    <View>
+      <Cat />
     </View>
   );
 };
 
+class Cat extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello, I am ...</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          defaultValue="Name me!"
+        />
+      </View>
+    );
+  }
+}
 export default HelloWorldApp;
