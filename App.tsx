8d682cea10b3d29287fb1f52c991d80087c5ee75
RajilSaj <svembe@gmail.com>
2023-04-03 01:15:19 -0400

    Splashscreen set

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View} from 'react-native';
+import {WithSplashScreen} from './components/splash';
 
 export default function App() {
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  useEffect(() => {
+    setIsAppReady(true);
+  }, []);
   return (
-    <View>
-      <Text>Hello World Devs Branch !</Text>
-    </View>
+    <WithSplashScreen isAppReady={isAppReady}>
+      <View>
+        <Text>Hello World Devs Branch !</Text>
+      </View>
+    </WithSplashScreen>
   );
 }
