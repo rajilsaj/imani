eb6a6d3d27c39f17c60cc74fa03219fafdff7a58
RajilSaj <svembe@gmail.com>
2023-04-04 02:49:24 -0400

    Fixed Imani  custom theme

--- screens/connexion/LoginScreen.tsx
+++ screens/connexion/LoginScreen.tsx
@@ -3,7 +3,7 @@ import {SafeAreaView, Text, View} from 'react-native';
 
 const LoginScreen = () => {
   return (
-    <SafeAreaView className="bg-green-900 h-full">
+    <SafeAreaView className="bg-azure-400 h-full">
       <View>
         <Text className="text-center mt-5 text-white">
           This is the login screen of my application
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,37 @@ module.exports = {
     './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        azure: {
+          50: '#f4f9fe',
+          100: '#e9f2fe',
+          200: '#c9dffc',
+          300: '#a9ccfa',
+          400: '#68a5f6',
+          500: '#277ff2',
+          600: '#2372da',
+          700: '#1d5fb6',
+          800: '#174c91',
+          900: '#133e77',
+        },
+        ebony: {
+          50: '#f4f4f5',
+          100: '#e9eaec',
+          200: '#c8cacf',
+          300: '#a7aab3',
+          400: '#666b79',
+          500: '#242b40',
+          600: '#20273a',
+          700: '#1b2030',
+          800: '#161a26',
+          900: '#12151f',
+        },
+      },
+    },
   },
   plugins: [],
 };
